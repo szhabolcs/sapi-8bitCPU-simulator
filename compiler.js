17051721ca79de9d959d9979612ddea20f692235
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-02-03 18:03:49 +0200

    endJmp label kinda works

--- compiler.js
+++ compiler.js
@@ -14,6 +14,7 @@ class Assembler {
   
       //find labels
       let finalCode=""
+      let index1=0;
 
       lines.forEach((line, index) => {
         let [opCode, operand, data] = line.split(/, |,| |:/).map(line => {
@@ -33,92 +34,123 @@ class Assembler {
         
         
         let firstChar = Assembler.hex(InstructionMap[opCode], "end");
-        let restOfString = Assembler.makeOperand(opCode, operand, data);
+        asmIndex+=1;
+        let restOfString = Assembler.makeOperand(opCode, operand, data, asmIndex);
         //console.log(restOfString,"\n");
        if(restOfString.length===1){
         finalCode =finalCode+" "+firstChar[0]+restOfString;
+        
        }
        else{
-        finalCode =finalCode+" "+firstChar[0]+restOfString[0]+restOfString.substring(restOfString.indexOf(" "));
-       } 
+        if(firstChar!==undefined){
+          finalCode = finalCode+" "+firstChar[0]+restOfString[0]+restOfString.substring(restOfString.indexOf(" "));
+        }
+        else{
+          finalCode = finalCode+" "+restOfString;
+        }
+        
+      } 
         //console.log(restOfString," ",restOfString.substring(restOfString.indexOf(" ")));
 
       });
-    
+      
       return finalCode;
      
     }
 
-    static makeOperand(opCode, operand, data){
+    static makeOperand(opCode, operand, data, asmIndex){
+      //asmIndex++;
         if(operand=='R0' && data=='R0'){
+          Assembler.asmIndex++;
           return '0';
         }
         else if(operand=='R0' && data=='R1'){
+          Assembler.asmIndex++;
           return '1';
         }
         else if(operand=='R0' && data=='R2'){
+          Assembler.asmIndex++;
           return '2';
         }
         else if(operand=='R0' && data=='R3'){
+          Assembler.asmIndex++;
           return '3';
         }
         else if(operand=='R1' && data=='R0'){
+          Assembler.asmIndex++;
           return '4';
         }
         else if(operand=='R1' && data=='R1'){
+          Assembler.asmIndex++;
           return '5';
         }
         else if(operand=='R1' && data=='R2'){
+          Assembler.asmIndex++;
           return '6';
         }
         else if(operand=='R1' && data=='R3'){
+          Assembler.asmIndex++;
           return '7';
         }
         else if(operand=='R2' && data=='R0'){
+          Assembler.asmIndex++;
           return '8';
         }
         else if(operand=='R2' && data=='R1'){
+          Assembler.asmIndex++;
           return '9';
         }
         else if(operand=='R2' && data=='R2'){
+          Assembler.asmIndex++;
           return 'a';
         }
         else if(operand=='R2' && data=='R3'){
+          Assembler.asmIndex++;
           return 'b';
         }
         else if(operand=='R3' && data=='R0'){
+          Assembler.asmIndex++;
           return 'c';
         }
         else if(operand=='R3' && data=='R1'){
+          Assembler.asmIndex++;
           return 'd';
         }
         else if(operand=='R3' && data=='R2'){
+          Assembler.asmIndex++;
           return 'e';
         }
         else if(operand=='R3' && data=='R3'){
+          Assembler.asmIndex++;
           return 'f';
         }
         else{
           if(operand=='R0'){
+            Assembler.asmIndex+=2;
             return '0 '+data.slice(-2);
           }
           else if(operand == 'R1'){
+            Assembler.asmIndex+=2;
             return'4 '+data.slice(-2);
           }
           else if(operand == 'R2'){
+            Assembler.asmIndex+=2;
             return'8 '+data.slice(-2);
           }
           else if(operand == 'R3'){
+            Assembler.asmIndex+=2;
             return'c '+data.slice(-2);
           }
           else{
-            console.log(opCode,"\n",operand,"\n",data,"\n");
-            return Assembler.hex(InstructionMap[operand], "start")+" "+Assembler.hex(Assembler.asmIndex, "start"); 
+            Assembler.asmIndex+=2;
+            //console.log(opCode,"\n",operand,"\n",data,"\n");
+            return Assembler.hex(InstructionMap[operand], "start")+" "+Assembler.hex(Assembler.asmIndex+1, "start"); 
           }
+          
         }
-        asmIndex+=1;
+        
     }
-
+    
 
 
 
@@ -161,7 +193,7 @@ function compile(testCode) {
 }
 
 
-let testCode = 'LI R2, 0x09\nLI R3, 0x07\nADD R3, R2\n';
-//let testCode = 'LI R2, 0x09\nLI R3, 0x07\nADD R3, R2\nend:JMP end';
+//let testCode = 'LI R2, 0x09\nLI R3, 0x07\nADD R3, R2\n';
+let testCode = 'LI R2, 0x09\nLI R3, 0x07\nADD R3, R2\nend:JMP end';
 //e8 09 ec 07 2e f0 05
 console.log(compile(testCode));
